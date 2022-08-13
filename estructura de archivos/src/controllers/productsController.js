bb1d5a89fb99db21df3b1025b9bcb40333f5f730
axellbustos <axell.bustos@hotmail.com.ar>
2022-08-13 15:52:34 -0300

    archivos productsLoad agregados

--- estructura de archivos/src/controllers/productsController.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    productDetail: (req, res) => {
-        return res.render('productDetail')
-    }
-}
--- estructura de archivos/src/routes/products.js
+++ estructura de archivos/src/routes/products.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
-const { productDetail } = require('../controllers/productsController')
+const { productDetail, productsLoad } = require('../controllers/productsController')
 
 /* /products */
 
 router.get('/productDetail', productDetail)
+router.get('/productsLoad', productsLoad)
 
 module.exports = router;
