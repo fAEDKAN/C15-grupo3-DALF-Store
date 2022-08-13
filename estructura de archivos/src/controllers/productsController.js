3f16b88dddaf2f08e1f4d31a1db9549c8fcc3ed0
axellbustos <axell.bustos@hotmail.com.ar>
2022-08-13 16:11:38 -0300

    problema con productsController.js solucionado

---
+++ estructura de archivos/src/controllers/productsController.js
@@ -0,0 +1,8 @@
+module.exports ={
+    productDetail: (req, res) => {
+        return res.render('productDetail')
+    },
+    productsLoad:(req,res)=> {
+        return res.render('productsLoad')
+    }
+}
