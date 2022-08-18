0a3a3f0d960af28ee346f75eb3922e68bba7c498
axellbustos <axell.bustos@hotmail.com.ar>
2022-08-17 22:42:34 -0300

    separar las vistas en carpetas done

--- estructura de archivos/src/controllers/productsController.js
+++ estructura de archivos/src/controllers/productsController.js
@@ -1,11 +1,21 @@
 module.exports = {
     productDetail: (req, res) => {
-        return res.render('productDetail')
+        return res.render('products/productDetail')
     },
     productsLoad: (req, res) => {
-        return res.render('productsLoad')
+        return res.render('products/productsLoad')
     },
     productEdit: (req, res) => {
-        return res.render('productEdit')
+        return res.render('products/productEdit')
+    },
+    cart: (req, res) => {
+        return res.render('products/cart')
+    },
+    /*ruta temporal*/ 
+    cartAdress: (req, res) => {
+        return res.render('products/cartAdress')
+    },
+    cartPay: (req, res) => {
+        return res.render('products/cartPay')
     }
 }
