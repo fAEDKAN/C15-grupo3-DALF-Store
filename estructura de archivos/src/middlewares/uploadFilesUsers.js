0bdcfe534aba7aea498b4ceff3a0c5737219cbaf
fAEDKAN <sfdk.94@hotmail.com>
2022-09-07 18:02:02 -0300

    partials logo added - users multer modified

--- estructura de archivos/src/middlewares/uploadFilesUsers.js
+++ estructura de archivos/src/middlewares/uploadFilesUsers.js
@@ -3,17 +3,22 @@
 const multer = require("multer");
 const path = require("path");
 
-const storage = multer.diskStorage({
- //Destino
- destination: (req, file, cb) => {
-  cb(null, "public/images/users");
- },
-  //Nombre
- filename: (req, file, cb) => {
-  cb(null, `user-${Date.now()}${path.extname(file.originalname)}`);
- },
+
+const storageUser = multer.diskStorage({
+
+    //Destino
+    destination: (req, file, cb) => {
+        cb(null, "/public/images/users");
+    },
+
+    //Nombre
+    filename: (req, file, cb) => {
+    cb(null, `user-${Date.now()}${path.extname(file.originalname)}`);
+    },
 });
-const uploadUser = multer({
- storage,
-});
-module.exports= uploadUser
+
+    const uploadUser = multer({
+    storageUser
+    });
+
+    module.exports = uploadUser
