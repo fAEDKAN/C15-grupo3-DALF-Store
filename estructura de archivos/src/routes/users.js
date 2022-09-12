876eba0ac305c3cb5a872377a89794054df4230e
fAEDKAN <sfdk.94@hotmail.com>
2022-09-12 17:05:08 -0300

    ...

--- estructura de archivos/src/routes/users.js
+++ estructura de archivos/src/routes/users.js
@@ -21,8 +21,8 @@ router.get('/logout', logout);
 router.get('/profile', userSessionCheck, profile);
 router.put('/update/:id', uploadUsers.single('avatar'), update);
 
-//MY SHOPPING
-router.get('/shopping', shopping);
+/* //MY SHOPPING
+router.get('/shopping', shopping); */
 
 
 module.exports = router;
