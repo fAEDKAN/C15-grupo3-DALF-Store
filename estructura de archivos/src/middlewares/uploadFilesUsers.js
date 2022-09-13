37726290a440faa76278588426220f9ef8196ea0
Diego Montes <ushiovii@hotmail.com>
2022-09-12 21:17:22 -0300

    code added and default-product-image added

--- estructura de archivos/src/middlewares/uploadFilesUsers.js
+++ estructura de archivos/src/middlewares/uploadFilesUsers.js
@@ -3,30 +3,28 @@ const multer = require("multer");
 const path = require("path");
 
 const storageUser = multer.diskStorage({
-
     //DESTINY
     destination: (req, file, cb) => {
-        cb(null, "/public/images/users");
+        cb(null, "./public/images/users");
     },
 
     //NAME
     filename: (req, file, cb) => {
-    cb(null, `user-${Date.now()}${path.extname(file.originalname)}`);
+        cb(null, `avatar-${Date.now()}${path.extname(file.originalname)}`);
     },
 });
 
-const fileFilter = (req, file, cb) => {
+/* const fileFilter = (req, file, cb) => {
     if(!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
         req.fileValidationError = "Sólo se permiten imágenes en formato .jpg, .jpeg y .png"
-        return cb(null, false, req.fileValidationError);
+        return cb(null, false, req.fileValidationError)
     }
-    return cb(null, true);
-};
+    return cb(null, true)
+ }; */
 
-    const uploadUsers = multer({
-        storage : storageUser,
-        fileFilter
-    });
+const uploadUsers = multer({
+    storage: storageUser
+    /* fileFilter */
+});
 
-
-module.exports = {uploadUsers};
+module.exports = { uploadUsers };
