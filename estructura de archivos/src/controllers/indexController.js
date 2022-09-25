c43a6857b443b8dd12b0d06fade4f55b8bceeb80
Diego Montes <ushiovii@hotmail.com>
2022-09-25 19:21:28 -0300

    code correction

--- estructura de archivos/src/controllers/indexController.js
+++ estructura de archivos/src/controllers/indexController.js
@@ -7,12 +7,12 @@ const controller  = {
     index: (req, res) => {
         let products = loadProducts();
         let productsInSale = products.filter(product => product.section === "in-sale");
-        let productsRecomended = products.filter(product => product.section === "recomended");
+        let productsRecommended = products.filter(product => product.section === "recommended");
         let productsOfUsers = products.filter(product => product.section === "of-users");
         return res.render('index',{
             products,
             productsInSale,
-            productsRecomended,
+            productsRecommended,
             productsOfUsers,
             toThousand
         });
