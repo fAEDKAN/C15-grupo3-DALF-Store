cf64c9ea74be2a88ae42f9bc46c7a8b29785de2f
fAEDKAN <sfdk.94@hotmail.com>
2022-12-16 03:40:06 -0300

    dash styles updated

--- estructura de archivos/admin/src/components/Footer.jsx
+++ estructura de archivos/admin/src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const Footer = () => {
 	return (
-		<footer className="sticky-footer bg-white">
+		<footer className="sticky-footer bg-white-footer">
 			<div className="container my-auto">
 				<div className="copyright text-center my-auto">
 					<span>Copyright &copy; DALF-Store 2022</span>
