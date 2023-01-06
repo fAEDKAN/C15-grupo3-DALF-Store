f9ed04b74f3bf52f1793d3750e1e604788f2e375
axell.bustos <axell.bustos@hotmail.com.ar>
2023-01-06 00:47:22 -0300

    contact routes

--- estructura de archivos/src/routes/index.js
+++ estructura de archivos/src/routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 //CONTROLLERS
-const {index, search, categoriesFilter, sectionsFilter, allSectionsProducts, brandsFilter} = require('../controllers/indexController');
+const {index, search, categoriesFilter, sectionsFilter, allSectionsProducts, brandsFilter, contact} = require('../controllers/indexController');
 
 //HOME
 router.get('/', index);
@@ -13,4 +13,7 @@ router.get('/categorias/:x',categoriesFilter)
 router.get('/section/:x', sectionsFilter)
 router.get('/allProducts', allSectionsProducts)
 router.get('/brand/:x', brandsFilter)
+
+//OTHERS
+router.get("/contact", contact)
 module.exports = router; 
